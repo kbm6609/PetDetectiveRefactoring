4f97ef2a62a33b57a95e3dd73db40cb7c2c9e0d9
kbm6609 <kbm6609@naver.com>
2022-07-27 20:25:40 +0900

    start

---
+++ refactoring/src/main/java/iospring/refactoring/RefactoringApplication.java
@@ -0,0 +1,13 @@
+package iospring.refactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringApplication.class, args);
+	}
+
+}
